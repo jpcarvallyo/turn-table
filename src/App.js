148dca5093dab8657205821e777ac5cdf26acc03
James Carvallyo <james.carvallyo@pearson.com>
2022-03-14 17:23:48 -0700

    Revert ":100: :rocket: Initial Commit (Set up repo)"
    This reverts commit 31caabdd90ef3710f0e08acedb5f58b09b234861.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import Header from './components/Header';
-import PadsContainer from './components/PadsContainer';
-import audioData from './data/audiometa.json';
-import './styles.css';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <PadsContainer data={audioData} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
