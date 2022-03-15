31caabdd90ef3710f0e08acedb5f58b09b234861
James Carvallyo <james.carvallyo@pearson.com>
2022-03-14 17:22:06 -0700

    :100: :rocket: Initial Commit (Set up repo)
    This is to initialize the repo.

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div className="header">
+      <header className="app-header">
+        <div>
+          <h1>Digital MIDI</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
